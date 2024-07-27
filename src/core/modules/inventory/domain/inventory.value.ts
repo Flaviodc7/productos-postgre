3e218849699afcf395ce9e431432120028d7c481
Flavio Coscarella <flaviodc@live.com.ar>
2024-07-27 11:48:45 -0300

    Fix: inventory value to meet new entities

--- src/core/modules/inventory/domain/inventory.value.ts
+++ src/core/modules/inventory/domain/inventory.value.ts
@@ -1,16 +1,50 @@
 import { v4 as uuid } from 'uuid';
-import { CreateInventoryPayload } from '@inventoryApplication/inventory.usecase.interface';
+import {
+  CreateInventoryPayload,
+  UpdateInventoryPayload,
+} from '@inventoryApplication/inventory.usecase.interface';
 import { InventoryEntity } from './entities/inventory.entity';
 
 export class InventoryValue {
   public create = (
     inventoryPayload: CreateInventoryPayload,
   ): InventoryEntity => {
+    const createdStatus = 'PENDING';
+
     return {
+      auditStatus: [
+        {
+          newStatus: createdStatus,
+          responsible: 'Admin', // TODO: responsible extracted from authentication user
+          updateDate: new Date().toISOString(),
+        },
+      ],
       id: uuid(),
-      status: 'PENDING',
+      currentStatus: createdStatus,
       createdAt: new Date().toISOString(),
       ...inventoryPayload,
+    };
+  };
+
+  public update = (
+    inventoryPayload: UpdateInventoryPayload,
+  ): InventoryEntity => {
+    const lastItemAuditStatus = inventoryPayload.auditStatus.length - 1;
+
+    const updatedAuditStatus = [
+      ...inventoryPayload.auditStatus,
+      {
+        newStatus: inventoryPayload.currentStatus,
+        previousStatus:
+          inventoryPayload.auditStatus[lastItemAuditStatus].newStatus,
+        responsible: 'Admin', // TODO: responsible extracted from authentication user
+        updateDate: new Date().toISOString(),
+      },
+    ];
+
+    return {
+      ...inventoryPayload,
+      auditStatus: updatedAuditStatus,
     };
   };
 }

504d12fb798b8da2b2a7e3dafa6282ee773652bb
Flavio Coscarella <flaviodc@live.com.ar>
2024-08-01 10:11:11 -0300

    Add: Docs to the DTO for Order Details and adding missing modules on main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { OrderDetailsModule } from '@modules/orderDetails.module';
 import { SubcategoryModule } from '@modules/subcategories.module';
+import { InventoryModule } from '@modules/inventory.module';
 import { CategoryModule } from '@modules/categories.module';
 import { ProductModule } from '@modules/products.module';
 import { DatabaseModule } from '@db/database.module';
 import { OrderModule } from '@modules/order.module';
 import { AppController } from '@src/app.controller';
+import { FilesModule } from '@modules/files.module';
 import { AppService } from '@src/app.service';
 import config from '@src/config';
 
@@ -17,10 +20,13 @@ import config from '@src/config';
       isGlobal: true,
     }),
     DatabaseModule,
-    ProductModule,
     CategoryModule,
-    SubcategoryModule,
+    FilesModule,
+    InventoryModule,
     OrderModule,
+    OrderDetailsModule,
+    ProductModule,
+    SubcategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/infra/dtos/orderDetails.dto.ts
+++ src/infra/dtos/orderDetails.dto.ts
@@ -3,70 +3,100 @@ import {
   IsArray,
   IsEnum,
   IsInt,
+  IsNotEmpty,
   IsNumber,
   IsOptional,
+  IsPositive,
   IsString,
+  Min,
   ValidateNested,
 } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { MethodType, PaymentStatus } from '../utils/enum.types';
 
 class OrderDeliveryDTO {
+  @ApiProperty({ description: 'Delivery Address' })
   @IsString()
+  @IsNotEmpty()
   address: string;
 
+  @ApiProperty({ description: 'Delivery City' })
   @IsString()
+  @IsNotEmpty()
   city: string;
 
+  @ApiProperty({ description: 'Delivery Postal Code' })
   @IsString()
+  @IsNotEmpty()
   postalCode: string;
 
+  @ApiProperty({ description: 'Delivery State' })
   @IsString()
+  @IsNotEmpty()
   state: string;
 
+  @ApiProperty({ description: 'Delivery Country' })
   @IsString()
+  @IsNotEmpty()
   country: string;
 }
 
 class OrderPaymentDTO {
+  @ApiProperty({ description: 'Payment Method' })
   @IsEnum(MethodType)
   method: MethodType;
 
+  @ApiProperty({ description: 'Payment Status' })
   @IsEnum(PaymentStatus)
   paymentStatus: PaymentStatus;
 }
 
 class OrderProductDTO {
+  @ApiProperty({ description: 'Product SKU' })
   @IsString()
+  @IsNotEmpty()
   sku: string;
 
+  @ApiProperty({ description: 'Product name' })
   @IsString()
+  @IsNotEmpty()
   name: string;
 
+  @ApiProperty({ description: 'Product Price' })
   @IsNumber()
+  @IsPositive()
   price: number;
 
+  @ApiProperty({ description: 'Product quantity' })
   @IsInt()
+  @Min(1)
   quantity: number;
 }
 
 export class UpdateOrderDetailsDTO {
+  @ApiProperty({ description: 'Order Details ID' })
   @IsString()
+  @IsNotEmpty()
   id: string;
 
+  @ApiProperty({ description: 'Order Details => Order ID' })
   @IsString()
+  @IsNotEmpty()
   orderId: string;
 
+  @ApiProperty({ description: 'Payment Details' })
   @ValidateNested()
   @Type(() => OrderPaymentDTO)
   payment: OrderPaymentDTO;
 
+  @ApiProperty({ description: 'List of Products' })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => OrderProductDTO)
   products: OrderProductDTO[];
 
+  @ApiPropertyOptional({ description: 'Delivery Details (optional)' })
   @IsOptional()
   @ValidateNested()
   @Type(() => OrderDeliveryDTO)
@@ -74,7 +104,7 @@ export class UpdateOrderDetailsDTO {
 }
 
 export class FindOrderDetailsIdsDTO {
-  @ApiProperty({ description: 'Order Details ID' })
+  @ApiProperty({ description: 'Order Details IDs' })
   @IsArray()
   @ArrayNotEmpty()
   @IsString({ each: true })
